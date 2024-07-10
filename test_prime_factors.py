c0010a264dcc641d94775f71db2694b6e9388ccd
Wontae Jeong <dev.jeong@samsung.com>
2024-07-10 09:44:03 +0900

    [refact] cleanup tests
    Signed-off-by: Wontae Jeong <dev.jeong@samsung.com>

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -8,23 +8,20 @@ class PrimeTest(unittest.TestCase):
         super().setUp()
         self.prime_factor = PrimeFactor()
 
-    def test_prime_factor_1(self):
-        self.assertEqual([], self.prime_factor.of(1))
-
-    def test_prime_factor_2(self):
-        self.assertEqual([2], self.prime_factor.of(2))
-
-    def test_prime_factor_3(self):
-        self.assertEqual([3], self.prime_factor.of(3))
-
-    def test_prime_factor_4(self):
-        self.assertEqual([2, 2], self.prime_factor.of(4))
-
-    def test_prime_factor_6(self):
-        self.assertEqual([2, 3], self.prime_factor.of(6))
-
-    def test_prime_factor_9(self):
-        self.assertEqual([3, 3], self.prime_factor.of(9))
-
-    def test_prime_factor_12(self):
-        self.assertEqual([2, 2, 3], self.prime_factor.of(12))
+    def test_prime_factor(self):
+        tests = [
+            {"target": 1, "expect": []},
+            {"target": 2, "expect": [2]},
+            {"target": 3, "expect": [3]},
+            {"target": 4, "expect": [2, 2]},
+            {"target": 9, "expect": [3, 3]},
+            {"target": 12, "expect": [2, 2, 3]},
+            {"target": 100, "expect": [2, 2, 5, 5]},
+            {"target": 1000, "expect": [2, 2, 2, 5, 5, 5]},
+            {"target": 23344342, "expect": [2, 7, 79, 21107]},
+            {"target": 233443242, "expect": [2, 3, 3, 3, 4323023]},
+            {"target": 2334432243, "expect": [3, 11, 13, 311, 17497]},
+        ]
+        for test in tests:
+            with self.subTest(f"factor {test["target"]}"):
+                self.assertEqual(test["expect"], self.prime_factor.of(test["target"]))
